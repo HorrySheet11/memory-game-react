52dee24ff713fbf04f52920e165fde111df1f3f4
HorrySheet11 <marinnathan11@gmail.com>
2025-12-05 19:15:55 +0800

    added header and cards
    styled elements with gradients
    added score and high score functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,41 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {pokemon} from './pokemon'
+
+const PokemonCard =({name, img}) => {
+  return (
+    <button type='button' key={name} className="card" onClick={() => setScore(score + 1)}>
+      <img src={img} alt={name} />
+      <h2>{name}</h2>
+    </button>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0)
+  const [highScore, setHighScore] = useState(0)
+  
+  if (score > highScore) {
+    setHighScore(score)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="logo-container">
+        <div>
+          <h1>Pokemon Memory Game</h1>
+        </div>
+        <div className="scores">
+          <h3>Score: {score}</h3>
+          <h3>High Score: {highScore}</h3>
+        </div>
+      </header>
+      <main className="card-grid">
+        {pokemon.map((poke) => (
+          <PokemonCard key={poke.name} name={poke.name} img={poke.img} />
+        ))}
+      </main>
     </>
   )
 }
---
+++ src/pokemon.js
@@ -0,0 +1,34 @@
+export const pokemon = [
+    {
+        name: "Metagross",
+        img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/376.png",
+    },
+    {
+        name: "Sneasler",
+        img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/903.png",
+    },
+    {
+        name: "Gardevoir",
+        img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/282.png",
+    },
+    {
+        name: "Lucario",
+        img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/448.png",
+    },
+    {
+        name: "Garchomp",
+        img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/445.png",
+    },
+    {
+        name: "Infernape",
+        img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/392.png",
+    },
+    {
+        name: "Swampert",
+        img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/260.png",
+    },
+    {
+        name: "Blaziken",
+        img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/257.png",
+    },
+]
